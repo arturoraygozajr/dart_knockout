79df5462cfcfb7980894c02f958aa2527f5beb53
Arturo Raygoza <arturoraygozajr@gmail.com>
2015-11-17 20:54:00 -0800

    created game lists

---
+++ dart_knockout.js
@@ -0,0 +1,45 @@
+Games = new Mongo.Collection("games");
+
+
+
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  //Session.setDefault('counter', 0);
+  //
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+
+
+
+  Template.body.helpers({
+    gamesx: function () {
+      result = Games.find({}).fetch();
+      return result;
+    }
+  });
+
+
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+
+
+}
+
+
+
+
+//if (Meteor.isServer) {
+//  Meteor.startup(function () {
+//    // code to run on server at startup
+//  });
+//}
